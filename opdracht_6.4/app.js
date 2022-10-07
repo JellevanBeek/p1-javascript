8d87064b4576bc93c6cae6d8d21ce2b8a691ccf9
JellevanBeek <9018326@student.zadkine.nl>
2022-10-07 09:33:46 +0200

    6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,30 @@
+
+
+
+function calculated(num, num2){
+let answer = num + num2;
+console.log(answer)
+}
+calculated(10, 19);
+
+
+function calculated2(num ){
+let answer2 = num * 7;
+console.log(answer2)
+}
+calculated2(5);
+
+function calculated3(num, num2){
+let answer3 = num / num2;
+console.log(answer3)
+}
+calculated3(3600, 60)
+
+
+
+
+
+
+
+
+
